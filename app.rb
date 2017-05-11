d0feef6abf72de597cae33b4a6cdd6ae80530dc0
Ben Metzger and zach beecher <ben+zach@>
2017-05-10 17:13:23 -0700

    add front end for adding recipes and ingredients

--- app.rb
+++ app.rb
@@ -9,7 +9,7 @@ get '/' do
   erb :index
 end
 
-post '/user' do
+post '/user/new' do
   name = params["name"]
   @user = User.new({:name => name})
   if @user.save()
@@ -19,17 +19,36 @@ post '/user' do
   end
 end
 
-
 get '/user/:user_id' do
   @user = User.find(params['user_id'].to_i())
   erb :user
 end
 
-post '/recipes/new' do
-  erb :create_recipe
+post '/user/:user_id/recipe/edit' do
+  name = params["name"]
+  @recipe = Recipe.new({:name => name})
+  @user = User.find(params['user_id'].to_i())
+  @user.recipes.push(@recipe)
+  erb :edit_recipe
 end
 
-get '/recipe/:recipe_id' do
+get '/user/:user_id/recipe/:recipe_id' do
+  @user = User.find(params['user_id'].to_i())
   @recipe = Recipe.find(params['recipe_id'].to_i())
+  erb :recipe
+end
+
+get '/user/:user_id/recipe/:recipe_id/ingredient/:ingredient_id' do
+  @user = User.find(params['user_id'].to_i())
+  @recipe = Recipe.find(params['recipe_id'].to_i())
+  @ingredient = Recipe.find(params['recipe_id'].to_i())
+  erb :edit_ingredient
+end
+
+patch '/user/:user_id/recipe/:recipe_id/add_ingredient' do
+  @user = User.find(params['user_id'].to_i())
+  @recipe = Recipe.find(params['recipe_id'].to_i())
+  name = params["name"]
+  ingredient = Ingredient.create({:name => name, :recipe_id => @recipe.id()})
   erb :recipe
 end
